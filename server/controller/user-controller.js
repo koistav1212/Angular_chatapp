0fa85e165687e40404a25a4562cc364ee5179684
koistav1212 <koustavsarkarapd@gmail.com>
2023-10-07 10:11:33 +0530

    chngs_build

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -4,189 +4,206 @@ const fs = require("fs");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      const imagesPath = "./uploads/userPics/" + req.params.id;
-  
-      if (!fs.existsSync(imagesPath)) {
-        fs.mkdirSync(imagesPath, { recursive: true });
-      }
-  
-      cb(null, imagesPath);
-    },
-    filename: (req, file, cb) => {
-      const profilePicFilename = "profilePic.jpg"; // Use a consistent filename
-      cb(null, profilePicFilename);
-    },
-  });
-  
-  const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1024 * 1024 * 10 }, // Limit file size to 10MB
-  }).single("profilePic");
+  destination: (req, file, cb) => {
+    const imagesPath = "./uploads/userPics/" + req.params.id;
 
+    if (!fs.existsSync(imagesPath)) {
+      fs.mkdirSync(imagesPath, { recursive: true });
+    }
+
+    cb(null, imagesPath);
+  },
+  filename: (req, file, cb) => {
+    const profilePicFilename = "profilePic.jpg"; // Use a consistent filename
+    cb(null, profilePicFilename);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 10 }, // Limit file size to 10MB
+}).single("profilePic");
 
 exports.updateUser = async (req, res) => {
-  upload(req, res, async (err) => {
-    if (err) {
-      res.status(500).json({
-        success: false,
-        message: err.message,
-      });
-    } else {
-      try {
-        const { _id, about } = req.body;
-        const profilePic = req.file ? "/" + req.file.path : ""; // Check if a file was uploaded
+  try {
+    upload(req, res, async (err) => {
+      if (err) {
+        throw new Error(err.message);
+      }
 
-        const user = await userSchema.findOneAndUpdate(
-          { _id: _id },
-          {
-            $set: {
-              profilePic: "http://localhost:5000"+profilePic,
-              about: about,
-            },
+      const { _id, about } = req.body;
+      const profilePic = req.file ? "/" + req.file.path : "";
+
+      const user = await userSchema.findOneAndUpdate(
+        { _id: _id },
+        {
+          $set: {
+            profilePic: "http://localhost:5000" + profilePic,
+            about: about,
           },
-          { new: true }
-        );
+        },
+        { new: true }
+      );
 
-        res.status(200).json({
-          success: true,
-          user,
-          message: "User updated successfully!",
-        });
-      } catch (error) {
-        res.status(400).json({
+      if (!user) {
+        return res.status(404).json({
           success: false,
-          message: error.message,
+          message: "User not found",
         });
       }
-    }
-  });
+
+      res.status(200).json({
+        success: true,
+        user,
+        message: "User updated successfully!",
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
 
-// >> Register Admin
+// Register Admin
 exports.createUser = async (req, res) => {
-    try {
-        const {
-            userName,
-            emailId,
-            password,
-            _id,
-            timestamps
-        } = req.body;
+  try {
+    const { userName, emailId, password, _id, timestamps } = req.body;
 
-        const user = await userSchema.create({
-            userName,
-            emailId,
-            password,
-            _id,
-            timestamps
-        })
-        res.status(200).json({
-            success: true,
-            user
-        })
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message
-        })
-    }
-}
+    const user = await userSchema.create({
+      userName,
+      emailId,
+      password,
+      _id,
+      timestamps,
+    });
 
-// >> Login Admin
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// Login Admin
 exports.userLogin = async (req, res) => {
+  try {
     const { emailId, password } = req.body;
 
     const user = await userSchema.findOne({ emailId });
-    console.log(user) 
+
     if (!user) {
-        res.status(401).json({
-            success: false,
-            message: "Invalid email or password"
-        })
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
 
-    const isPasswordMatched = await user.comparePassword(password)
+    const isPasswordMatched = await user.comparePassword(password);
 
     if (!isPasswordMatched) {
-        res.status(401).json({
-            success: false,
-            message: "Invalid email or password"
-        })
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
     }
-else
-    res.status(200).json({
-        success: true,
-        user: "user",
-        user
-    })
-}
 
-//get All users
-exports.allUsers = async (req, res) => {
-console.log(req.body)
-    const userList = await userSchema.find({ _id: { $ne: req.body.id } });
-  
-    if (!userList || userList.length === 0) {
-        res.status(401).json({
-            success: false,
-            message: "No users found"
-        })
-    }
-else{
-   
     res.status(200).json({
-        success: true,
-        user: "user",
-        userList
-    })
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
   }
-}
-exports.getUserbyID = async (req, res) => {
+};
 
-    const user = await userSchema.findOne({ _id:req.params.id })
-    
-    if (!user) {
-        res.status(401).json({
-            success: false,
-            message: "No users found"
-        })
+// Get All users
+exports.allUsers = async (req, res) => {
+  try {
+    const userList = await userSchema.find({ _id: { $ne: req.body.id } });
+
+    if (!userList || userList.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No users found",
+      });
     }
 
-   
     res.status(200).json({
-        success: true,
-        type: "user",
-        user
-    })
-   
-}
+      success: true,
+      userList,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
 
-//update user
+exports.getUserbyID = async (req, res) => {
+  try {
+    const user = await userSchema.findOne({ _id: req.params.id });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "No users found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+// Update user
 exports.updateconnectionsUser = async (req, res) => {
-    const {
-        connections,
-        conversations
-    } = req.body;
+  try {
+    const { connections, conversations } = req.body;
 
-    const user = userSchema.findByIdAndUpdate(req.params.id, req.body).then((res1)=>{
+    const user = await userSchema.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-        res.status(200).json({
-            success: true,
-            message: "User Updated Successfully!",
-            user
-        })
-    })
-    .catch((reason)=>{
-        res.status(401).json({
-            success: false,
-            message: "User Not Found!",
-        
-        })
-    }) 
-    
-        
-    
-      
-    
-}
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User Updated Successfully!",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
